950a6cb9be4519e575796c4ecdeb19271fe2e013
Damian Betebenner <dbetebenner@nciea.org>
2023-10-17 14:18:54 -0400

    Tidying up

--- WIDA_NM_Growth_Model_Output.R
+++ WIDA_NM_Growth_Model_Output.R
@@ -8,7 +8,8 @@
 require(data.table)
 
 ### Load data
-load("Data/WIDA_NM_SGP_LONG_Data.Rdata")
+#load("Data/WIDA_NM_SGP_LONG_Data.Rdata") ## Output with SGP data in addition
+load("Data/WIDA_NM_Data_LONG.Rdata")
 
 ### Variables to create summaries for
 variable.names.2023.demo <- c("Female", "Male", "Hispanic", "White", "Black", "Asian", "Native", "Multirace", "FRL", "NotFRL", "DirectCert", "NotDirectCert", "SwD", "NotSwD", "EL", "NotEL", "Migrant", "Homeless", "Military", "Foster", "Any_Native", "Any_Black")
@@ -21,8 +22,6 @@ percent_in_category <- function(my.variable, in.categories, all.categories) {
     my.table <- table(my.variable)
     return(round(100*sum(my.table[in.categories])/sum(my.table[all.categories]), digits=1))
 }
-
-
 
 ### ALL Students 
 WIDA_NM_Summaries_LIST[["All_Students"]] <- data.table(Group="All Students", Percent_Meeting_or_Exceeding_Target=percent_in_category(WIDA_NM_SGP_LONG_Data[['NEW_MEXICO_GROWTH_MODEL_TARGET_MET']], "Met New Mexico Growth Target" , c("Did not meet New Mexico Growth Target", "Met New Mexico Growth Target")), Count=sum(!is.na(WIDA_NM_SGP_LONG_Data[['NEW_MEXICO_GROWTH_MODEL_TARGET_MET']])))
