cdd47ee2690cce8a2a551cf65c1265fbb07c3d41
Damian Betebenner <dbetebenner@nciea.org>
2023-10-17 10:33:19 -0400

    Push code to GitHub

--- SGP_CONFIG/2023/READING.R
+++ SGP_CONFIG/2023/READING.R
@@ -8,5 +8,5 @@ READING_2023.config <- list(
 	READING.2023 = list(
 		sgp.content.areas=rep('READING', 2),
 		sgp.panel.years=c('2022', '2023'),
-		sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12'))))
+		sgp.grade.sequences=list(c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12'))))
 
